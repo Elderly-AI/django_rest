5b3533d6607b005119be01566d8d572236b4bb77
awerkiler <aae_pl@mail.ru>
2021-03-19 22:14:58 +0300

    add comments

--- speech/settings.py
+++ speech/settings.py
@@ -130,4 +130,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ],
+    #'DEFAULT_RENDERER_CLASSES': (
+    #    'rest_framework.renderers.JSONRenderer',
+    #)
 }
