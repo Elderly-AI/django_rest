bdaaf8a5054c779009c2dc04bb85652f4fbb786a
Romanecz00 <romanecz00@gmail.com>
2021-03-29 22:40:59 +0300

    making async consumers, look for TODO in chat/consumers.py

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,17 +1,35 @@
 import json
 from channels.generic.websocket import WebsocketConsumer
+from channels.consumer import AsyncConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 
-class ChatConsumer(WebsocketConsumer):
-    def connect(self):
-        self.accept()
+class MyConsumer(AsyncWebsocketConsumer):
+    groups = ["broadcast"]
 
-    def disconnect(self, close_code):
-        pass
+    async def connect(self):
+        # Called on connection.
+        # To accept the connection call:
+        await self.accept()
+        # Or accept the connection and specify a chosen subprotocol.
+        # A list of subprotocols specified by the connecting client
+        # will be available in self.scope['subprotocols']
+        # await self.accept("subprotocol")
+        # To reject the connection, call:
+        await self.close()
 
-    def receive(self, text_data):
+    async def receive(self, text_data=None, bytes_data=None):
+        # Called with either text_data or bytes_data for each frame
+        # You can call:
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
 
-        self.send(text_data=json.dumps({
+        await self.send(text_data=json.dumps({
             'message': message
         }))
+        # Want to force-close the connection? Call:
+        await self.close()
+        # Or add a custom WebSocket error code!
+        await self.close(code=4123)
+
+    async def disconnect(self, close_code):
+        pass  # TODO write this shit
