adb17db181090498047727638cec7d364a2d9de9
Romanecz00 <romanecz00@gmail.com>
2021-03-29 22:17:17 +0300

    working

--- speech/asgi.py
+++ speech/asgi.py
@@ -12,7 +12,7 @@ import os
 from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Logoped.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'speech.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
--- speech/settings.py
+++ speech/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'channels',
     'chat',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -40,7 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'user',
-    'channels',
     'profiles'
 ]
 
